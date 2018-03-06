bb77725c3caab2dc42f89327a0f2382972b8e37e
Simon Hočevar <simonhocevar@simons-macbook-pro.local>
2018-03-06 18:08:24 +0100

    Initial Commit

---
+++ Rain Radar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rain Radar
+//
+//  Created by Simon Hočevar on 06/03/2018.
+//  Copyright © 2018 Simon Hočevar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Rain RadarTests/Rain_RadarTests.swift
@@ -0,0 +1,36 @@
+//
+//  Rain_RadarTests.swift
+//  Rain RadarTests
+//
+//  Created by Simon Hočevar on 06/03/2018.
+//  Copyright © 2018 Simon Hočevar. All rights reserved.
+//
+
+import XCTest
+@testable import Rain_Radar
+
+class Rain_RadarTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
